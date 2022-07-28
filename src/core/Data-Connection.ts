d97f1752b71715539b35873c8b3f6f49aa7f3c18
Francisco Javier Santiago Muñoz <frangarmu95@gmail.com>
2022-07-28 18:04:48 -0500

    archivo .env de configuración

--- src/core/Data-Connection.ts
+++ src/core/Data-Connection.ts
@@ -4,14 +4,16 @@ import { Roles } from "../schemas/Roles"
 import { Users } from "../schemas/Users"
 import { Vehicles } from "../schemas/Vehicles"
 import { Branchs } from "../schemas/Branchs"
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: process.env.hostDB,
     port: 3306,
-    username: "root",
-    password: "",
-    database: "logistic",
+    username: process.env.userDB,
+    password: process.env.passDB,
+    database: process.env.nameDB,
     entities: [Roles,Users,Vehicles,Branchs],
     synchronize: true,
     logging: false,
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 const routes = require("./core/routes");
+const cors = require("cors");
 
 dotenv.config();
-// console.log(process.env.localhost)
+// console.log(process.env.userDB)
 const app: Express = express();
 const port = process.env.PORT;
 
 app.set("port", port || 3000);
 // app.use(express.urlencoded({ extended: true, strict: false }));
 app.use(express.json());
-
+app.use(cors());
 app.use("/v1", routes);
 
 // app.get('/', (req: Request, res: Response) => {
